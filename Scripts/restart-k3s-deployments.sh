87064b60d18b346f671e4901a7d381424ef391d2
Colin <58808821+codefuturist@users.noreply.github.com>
2025-01-22 09:42:01 +0100

    Update restart-k3s-deployments.sh

--- Scripts/restart-k3s-deployments.sh
+++ Scripts/restart-k3s-deployments.sh
@@ -1,1 +1,24 @@
 
+kubectl scale deployment/erpnext-gunicorn   --replicas=0 -n experimental
+kubectl scale deployment/erpnext-nginx      --replicas=0 -n experimental
+kubectl scale deployment/erpnext-scheduler  --replicas=0 -n experimental
+kubectl scale deployment/erpnext-socketio   --replicas=0 -n experimental
+kubectl scale deployment/erpnext-worker-d   --replicas=0 -n experimental
+kubectl scale deployment/erpnext-worker-l   --replicas=0 -n experimental
+kubectl scale deployment/erpnext-worker-s   --replicas=0 -n experimental
+
+kubectl scale statefulset/mariadb-primary --replicas=0 -n database
+kubectl scale statefulset/postgresql-primary --replicas=0 -n database
+
+sleep 120
+
+kubectl scale deployment/erpnext-gunicorn   --replicas=1 -n experimental
+kubectl scale deployment/erpnext-nginx      --replicas=1 -n experimental
+kubectl scale deployment/erpnext-scheduler  --replicas=1 -n experimental
+kubectl scale deployment/erpnext-socketio   --replicas=1 -n experimental
+kubectl scale deployment/erpnext-worker-d   --replicas=1 -n experimental
+kubectl scale deployment/erpnext-worker-l   --replicas=1 -n experimental
+kubectl scale deployment/erpnext-worker-s   --replicas=1 -n experimental
+
+kubectl scale statefulset/mariadb-primary --replicas=1 -n database
+kubectl scale statefulset/postgresql-primary --replicas=1 -n database
