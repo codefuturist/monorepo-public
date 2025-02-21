326bb1f0f2d7862338492555c761c70811dfa642
Colin <58808821+codefuturist@users.noreply.github.com>
2025-02-21 06:30:32 +0100

    Update basic-ubuntu-setup.sh

--- Scripts/basic-ubuntu-setup.sh
+++ Scripts/basic-ubuntu-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt update
-apt install qemu-guest-agent nfs-common lvm2 -y
+apt install qemu-guest-agent nfs-common lvm2 spice-vdagent -y
 timedatectl set-timezone Europe/Zurich
 ufw disable
 reboot
