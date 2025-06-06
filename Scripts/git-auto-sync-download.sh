9ef27365bb32e040a0b923022199b86efcf49e70
Colin <58808821+codefuturist@users.noreply.github.com>
2025-06-06 09:44:26 +0200

    Update git-auto-sync-download.sh

--- Scripts/git-auto-sync-download.sh
+++ Scripts/git-auto-sync-download.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env zsh
 
 wget https://raw.githubusercontent.com/codefuturist/monorepo-public/refs/heads/main/Scripts/git-auto-sync2.sh
 # curl -O https://raw.githubusercontent.com/codefuturist/monorepo-public/refs/heads/main/Scripts/git-auto-sync2.sh
