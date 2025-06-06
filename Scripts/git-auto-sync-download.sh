11449287a83e5e589b7d0e9525d59d676618bd77
Colin <58808821+codefuturist@users.noreply.github.com>
2025-06-06 10:44:51 +0200

    Update git-auto-sync-download.sh

--- Scripts/git-auto-sync-download.sh
+++ Scripts/git-auto-sync-download.sh
@@ -8,6 +8,8 @@ USER="${SCRIPT_USER:-$USER}"
 
 curl -fsSL "$SCRIPT_URL" -o "/tmp/$SCRIPT_NAME" || { echo "Download failed"; exit 1; }
 
+# wget -q -O "$TMP_FILE" "$SCRIPT_URL" || { echo "Download failed"; exit 1; }
+
 if [ "/tmp/$SCRIPT_NAME" != "$DEST_PATH" ]; then
   install -o "$USER" -g "$USER" -m 777 "/tmp/$SCRIPT_NAME" "$DEST_PATH"
 else
